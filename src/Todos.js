09532645e71dbd5822ef3da809b89b8f3c971bbc
HERALDEXX <inyangheraldmbre@outlook.com>
2024-10-25 16:51:37 +0100

    useCallback, useMemo & Custom Hooks

--- src/Todos.js
+++ src/Todos.js
@@ -1,6 +1,18 @@
 import { memo } from "react";
 
-const Todos = ({ todos }) => {
+// const Todos = ({ todos, addTodo }) => {
+//   console.log("child render");
+//   return (
+//     <>
+//       <h2>My Todos</h2>
+//       {todos.map((todo, index) => {
+//         return <p key={index}>{todo}</p>;
+//       })}
+//       <button onClick={addTodo}>Add Todo</button>
+//     </>
+//   );
+// };
+const Todos = ({ todos, addTodo }) => {
   console.log("child render");
   return (
     <>
@@ -8,8 +20,8 @@ const Todos = ({ todos }) => {
       {todos.map((todo, index) => {
         return <p key={index}>{todo}</p>;
       })}
+      <button onClick={addTodo}>Add Todo</button>
     </>
   );
 };
-
 export default memo(Todos);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { useState, useCallback } from "react";
 import ReactDOM from "react-dom/client";
+//import Todos from "./Todos";
+import useFetch from "./useFetch";
 
-function Timer() {
-  const [count, setCount] = useState(100);
+/*
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setCount((count) => count - 1);
-    }, 1000);
-  });
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
 
-  return <h1>Rendering will stop in {count} seconds!</h1>;
-}
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+*/
 
+/*
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = useCallback(() => {
+    setTodos((t) => [...t, "New Todo"]);
+  }, [todos]);
+
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+*/
+
+const Home = () => {
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
+
+  return (
+    <>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
+    </>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Timer />);
+// root.render(<Timer />);
+//root.render(<App />);
+root.render(<Home />);
